95922d7ec7050bea946c4ab02f5c7ee54426a382
123 <84119280+helloobaby@users.noreply.github.com>
2022-04-10 20:19:34 +0800

    Create test.cc

---
+++ test.cc
@@ -0,0 +1,27 @@
+#include "align.hh"
+
+#include <iostream>
+struct ETHREAD
+{
+    union
+    {
+        struct 
+        {
+            make_offset(0x4C8);
+            void* Cid[2];
+        };
+        struct 
+        {
+            make_offset(0x560);
+            uint32_t CrossThreadFlags;
+        };
+    };
+};
+
+int main()
+{
+    std::cout << std::hex;
+    std::cout << "offset Cid is : " << offsetof(ETHREAD,Cid) << std::endl; 
+    std::cout << "offset Flags is : " << offsetof(ETHREAD,CrossThreadFlags) << std::endl; 
+    return 0;
+}
