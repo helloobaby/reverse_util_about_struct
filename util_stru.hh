78936946805adccace5a6cfa3f0f151df87a4582
sbb <84119280+helloobaby@users.noreply.github.com>
2022-06-25 15:57:07 +0800

    Update util_stru.hh

--- util_stru.hh
+++ util_stru.hh
@@ -1,6 +1,8 @@
 #pragma once
-#include<cstddef>
 
 #define STR_MERGE_IMPL(a, b) a##b
 #define STR_MERGE(a, b) STR_MERGE_IMPL(a, b)
-#define make_offset(offset) std::byte STR_MERGE(pad,__COUNTER__)[offset]
+#define make_offset(offset) char STR_MERGE(pad,__COUNTER__)[offset]
+
+//for IDA 
+#define make_offset_n(offset,name) char STR_MERGE(pad,name)[offset]
